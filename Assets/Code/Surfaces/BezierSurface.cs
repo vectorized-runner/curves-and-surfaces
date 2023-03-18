2be4635729425cefa0f43e5acf0c63ba49b4fd83
Bartu Baş <bartubas99@gmail.com>
2023-03-18 14:09:46 +0300

    WIP: Bezier surface

---
+++ Assets/Code/Surfaces/BezierSurface.cs
@@ -0,0 +1,68 @@
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Surfaces
+{
+	public readonly struct BezierSurface
+	{
+		public readonly float3 P1;
+		public readonly float3 P2;
+		public readonly float3 P3;
+		public readonly float3 P4;
+
+		public BezierSurface(float3 p1, float3 p2, float3 p3, float3 p4)
+		{
+			P1 = p1;
+			P2 = p2;
+			P3 = p3;
+			P4 = p4;
+		}
+
+		public float3 SamplePoint(float s, float t)
+		{
+			Debug.Assert(s >= 0f && s <= 1f);
+			Debug.Assert(t >= 0f && t <= 1f);
+
+			var points4 = new float3[] { P1, P2, P3, P4 };
+			var result = float3.zero;
+
+			for (int i = 0; i <= 3; i++)
+			for (int j = 0; j <= 3; j++)
+			{
+				result += points4[i] * Bernstein(3, i, s) * Bernstein(3, j, t);
+			}
+
+			return result;
+		}
+
+		float3 Bernstein(int n, int i, float t)
+		{
+			return Combination(n, i) * Pow(t, i) * Pow(1 - t, n - i);
+		}
+
+		float Pow(float t, int i)
+		{
+			return math.pow(t, i);
+		}
+
+		int Combination(int n, int r)
+		{
+			return Factorial(n) / (Factorial(r) * Factorial(n - r));
+		}
+
+		int Factorial(int n)
+		{
+			Debug.Assert(n >= 0);
+
+			var res = 1;
+
+			while (n > 1)
+			{
+				res *= n;
+				n--;
+			}
+
+			return res;
+		}
+	}
+}
---
+++ Assets/Code/Surfaces/BezierSurfaceDrawer.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+namespace Surfaces
+{
+	public class BezierSurfaceDrawer : MonoBehaviour
+	{
+		public Transform P1;
+		public Transform P2;
+		public Transform P3;
+		public Transform P4;
+		public int SampleAmount = 100;
+		public Color DrawColor = Color.yellow;
+
+		void Update()
+		{
+			if (SampleAmount <= 0)
+			{
+				Debug.LogError("SampleAmount should be positive.");
+				return;
+			}
+
+			var surface = new BezierSurface(
+				P1.transform.position,
+				P2.transform.position,
+				P3.transform.position,
+				P4.transform.position);
+
+			var previousPoint = surface.SamplePoint(0f, 0f);
+
+			for (float t = 0f; t <= 1f; t += 1f / SampleAmount)
+			{
+				for (float s = 0f; s <= 1f; s += 1f / SampleAmount)
+				{
+					Debug.Log($"SamplePoint s: {s}, t: {t}");
+				
+					var point = surface.SamplePoint(s, t);
+
+					Debug.DrawLine(previousPoint, point, DrawColor);
+
+					previousPoint = point;
+				}
+				
+				Debug.DrawLine(previousPoint, surface.SamplePoint(1f, t), DrawColor);
+				
+				Debug.Log($"SamplePoint s: {1f}, t: {t}");
+			}
+
+
+			Debug.DrawLine(previousPoint, surface.SamplePoint(1f, 1f), DrawColor);
+
+			Debug.Log("Drawing");
+		}
+	}
+}
